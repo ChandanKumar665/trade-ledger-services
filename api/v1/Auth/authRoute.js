5b0c2acacb570a3d4287c265d260f409185794b2
ck6652265 <ck6652265@gmail.com>
2026-08-22 00:08:34 +0530

    fixes

--- api/v1/Auth/authRoute.js
+++ api/v1/Auth/authRoute.js
@@ -5,6 +5,13 @@ const AuthCtrl = require('./authCtrl')
 //routes
 router.post('/login', new AuthCtrl().login)
 router.get('/me', auth, new AuthCtrl().checkMe)
+router.get('/health-check', (req, res) => {
+    res.status(200).json({
+        message: `OK`,
+        success: true,
+        statusCode: 200
+    })
+})
 router.post('/signup', new AuthCtrl().signup)
 router.post('/logout', new AuthCtrl().logout)
 
