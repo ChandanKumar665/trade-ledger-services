599961f188b42aec7e286ea685707eacc7941fab
ck6652265 <ck6652265@gmail.com>
2026-08-23 23:30:26 +0530

    debug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,6 +5,7 @@ const AppError = require('../utils/AppError');
 
 const auth = async (req, res, next) => {
     try {
+        console.log("PATH--------->", req.path);
         console.log("COOKIE HEADER--------->", req.headers.cookie);
         console.log("PARSED COOKIES-------->", req.cookies);
         const { token } = req.cookies;
